83e8a26c7080b3016f1aa132fcf48280fa6b10c8
vis-prime <vishals1030@gmail.com>
2023-07-05 00:13:57 +0530

    files

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite"
+import viteCompression from "vite-plugin-compression"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+  base: "./",
+  server: {
+    port: 3000,
+  },
+  plugins: [viteCompression()],
+})
